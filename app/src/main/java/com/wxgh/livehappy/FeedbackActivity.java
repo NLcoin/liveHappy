115e4351d5342af15e21f79775e9bad89ffad804
Tony <wstcdls@126.com>
2016-07-15 20:57:08 +0800

    反馈页面

---
+++ app/src/main/java/com/wxgh/livehappy/FeedbackActivity.java
@@ -0,0 +1,16 @@
+package com.wxgh.livehappy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 反馈页面
+ */
+public class FeedbackActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feedback);
+    }
+}
