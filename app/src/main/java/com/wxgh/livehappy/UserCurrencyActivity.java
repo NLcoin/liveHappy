e4a4efa88b2481cc5fe9ae7b5f171c4a30750c74
Tony <wstcdls@126.com>
2016-07-19 17:48:34 +0800

    关注、粉丝、好友通用UserCurrencyActivity

---
+++ app/src/main/java/com/wxgh/livehappy/UserCurrencyActivity.java
@@ -0,0 +1,16 @@
+package com.wxgh.livehappy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserCurrencyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_currency);
+
+//        对应的行布局  item_user_currency_layout
+//        imageview控件的图片说明：plusfollow加关注  cancelfollow取消关注
+    }
+}
